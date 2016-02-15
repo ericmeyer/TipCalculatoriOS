0c5516103e608d46b7ad1345b4afa67e03d2d0b8
Eric Meyer <emeyer@8thlight.com>
2016-02-15 06:34:28 -0600

    Add empty framework target

---
+++ TipCalculator/TipCalculator.h
@@ -0,0 +1,19 @@
+//
+//  TipCalculator.h
+//  TipCalculator
+//
+//  Created by Eric Meyer on 2/15/16.
+//  Copyright © 2016 Eric Meyer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TipCalculator.
+FOUNDATION_EXPORT double TipCalculatorVersionNumber;
+
+//! Project version string for TipCalculator.
+FOUNDATION_EXPORT const unsigned char TipCalculatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TipCalculator/PublicHeader.h>
+
+
