dceda2689a5e28d932232d5b937a31e7519a4e2b
Eric Meyer <emeyer@8thlight.com>
2016-03-12 10:31:33 -0600

    Start of iPhone layout and view

--- TipCalculatoriOS/ViewController.swift
+++ TipCalculatoriOS/ViewController.swift
@@ -1,17 +1,32 @@
 import UIKit
+import TipCalculator
 
 public class ViewController: UIViewController {
 
+    @IBOutlet weak public var tipAmount: UILabel!
+    @IBOutlet weak public var tipPercent: UISlider!
+    private var subtotal: Float = 0.0
+    private var bill = Bill()
+
     override public func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        bill.tipPercentage = 15.0
+        tipPercent.value = 15.0
+        updateTipAmount()
     }
 
-    override public func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction public func subtotalChanged(sender: UITextField) {
+        bill.subtotal = (sender.text! as NSString).floatValue
+        updateTipAmount()
     }
 
+    public func tipPercentChanged(tipPercent: Float){
+        bill.tipPercentage = tipPercent
+        updateTipAmount()
+    }
+
+    private func updateTipAmount() {
+        tipAmount.text = CurrencyFormatter().format(bill.tip())
+    }
 
 }
-
--- TipCalculatoriOSSpecs/ViewControllerSpec.swift
+++ TipCalculatoriOSSpecs/ViewControllerSpec.swift
@@ -5,9 +5,97 @@ import TipCalculatoriOS
 class ViewControllerSpec: QuickSpec {
     override func spec() {
 
-        it("exists") {
-            let controller = ViewController()
-            expect(controller).notTo(beNil())
+        var controller: ViewController!
+        var tipAmount: UILabel!
+        var tipPercent: UISlider!
+
+        beforeEach({
+            controller = ViewController()
+            tipAmount = UILabel()
+            controller.tipAmount = tipAmount
+
+            tipPercent = UISlider()
+            tipPercent.maximumValue = 30.0
+            controller.tipPercent = tipPercent
+        })
+
+        describe("Loading the view") {
+            it("starts with an initial tip amount of $0") {
+                controller.viewDidLoad()
+
+                expect(tipAmount.text).to(equal("$0.00"))
+            }
+
+            it("starts with a tip percent of 15%") {
+                controller.viewDidLoad()
+
+                expect(tipPercent.value).to(equal(15.0))
+            }
         }
+
+        describe("Updating the subtotal") {
+            var subtotalInput: UITextField!
+
+            beforeEach({
+                subtotalInput = UITextField()
+            })
+
+            it("calculates a new tip for $100") {
+                controller.viewDidLoad()
+                subtotalInput.text = "100.0"
+
+                controller.subtotalChanged(subtotalInput)
+
+                expect(tipAmount.text).to(equal("$15.00"))
+            }
+
+            it("calculates a new tip for $200") {
+                controller.viewDidLoad()
+                subtotalInput.text = "200.0"
+
+                controller.subtotalChanged(subtotalInput)
+
+                expect(tipAmount.text).to(equal("$30.00"))
+            }
+        }
+
+        describe("Updating the tip percent") {
+            var subtotalInput: UITextField!
+
+            beforeEach({
+                subtotalInput = UITextField()
+            })
+
+            it("recalculates the tip for a big tipper") {
+                controller.viewDidLoad()
+                subtotalInput.text = "100.0"
+                controller.subtotalChanged(subtotalInput)
+
+                controller.tipPercentChanged(20.0)
+
+                expect(tipAmount.text).to(equal("$20.00"))
+            }
+
+            it("recalculates the tip for a poor service") {
+                controller.viewDidLoad()
+                subtotalInput.text = "100.0"
+                controller.subtotalChanged(subtotalInput)
+
+                controller.tipPercentChanged(5.0)
+
+                expect(tipAmount.text).to(equal("$5.00"))
+            }
+
+            it("recalculates the tip for a different subtotal") {
+                controller.viewDidLoad()
+                subtotalInput.text = "200.0"
+                controller.subtotalChanged(subtotalInput)
+
+                controller.tipPercentChanged(10.0)
+
+                expect(tipAmount.text).to(equal("$20.00"))
+            }
+        }
+
     }
 }
